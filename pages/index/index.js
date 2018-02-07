40cac9ef69860b11c81020b4bca391795d2e80a5
JerryCaicos <1287594398@qq.com>
2018-02-07 18:54:00 +0800

    scroll

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,42 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
+    condition:true,
+    toView: 'red',
+    scrollTop: 100,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  },
+  
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
